0beabeeeab0790d05e03005c1960d1e311fbb86b
skous2 <tanner.skousen@gmail.com>
2023-10-12 23:26:43 -0600

    add bivariate

---
+++ greetings.py
@@ -0,0 +1,11 @@
+def english(name):
+    return f'Hi {name}!'
+
+def spanish(name):
+    return f'Hola {name}!'
+
+def french(name):
+    return f'Bonjour {name}!'
+
+def german(name):
+    return f'Hallo {name}!'
---
+++ greetings2.py
@@ -0,0 +1,14 @@
+def spanish(greeting):
+    return f'Hola {greeting}!'
+
+def mandarin(greeting):
+    return f'Ni hao {greeting}!'
+
+def french(greeting):
+    return f'Bonjour {greeting}!'
+
+def german(greeting):
+    return f'Hallo {greeting}!'
+
+def english(greeting):
+    return f'Hi {greeting}!'
